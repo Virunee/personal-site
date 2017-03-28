01c2a9a5bfe90c8ae9291099343da163a9239b24
Virunee <viravongv@gmail.com>
2017-03-28 23:30:32 +0100

    Added filler content
    Set up routing to each page and gave some initial page content

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ personalSite.config(function ($routeProvider) {
         templateUrl: 'pages/main.html',
         controller: 'mainController'
   })
+
+    .when('/about', {
+      templateUrl: 'pages/about.html'
+    })
+
+    .when('/developer', {
+      templateUrl: 'pages/developer.html'
+    })
+
+    .when('/art', {
+      templateUrl: 'pages/art.html'
+    })
+
+    .when('/videos', {
+      templateUrl: 'pages/videos.html'
+    })
+
+    .when('/podcasts', {
+      templateUrl: 'pages/podcasts.html'
+    })
 });
 
 //CONTROLLERS
